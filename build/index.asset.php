06dab25cd3922702727bd310bfcb242800c5ec3b
Jason Adams <jason.adams@impress.org>
2022-02-04 13:16:24 -0800

    chore: ignore build files

--- build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => 'c4c2b897a6370f485b1a619629da3bfc');
