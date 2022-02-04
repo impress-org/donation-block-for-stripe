f3f9f1cee13c33cf5af461d4b969db78062ad433
Jason Adams <jason.adams@impress.org>
2022-02-04 13:54:14 -0800

    feature: add stripe connect handling

--- donation-block-for-stripe.php
+++ donation-block-for-stripe.php
@@ -16,6 +16,8 @@
 
 define('DONATION_BLOCK_SCRIPT_ASSET', require(plugin_dir_path(__FILE__) . 'build/index.asset.php'));
 
+require_once 'vendor/autoload.php';
+
 /**
  * @return void
  */
@@ -27,16 +29,16 @@ function create_donation_form_block_init()
 	}
 
 	wp_register_script(
-			'donation-form-block-script',
-			plugins_url('build/index.js', __FILE__),
-			DONATION_BLOCK_SCRIPT_ASSET['dependencies'],
-			DONATION_BLOCK_SCRIPT_ASSET['version']
+		'donation-form-block-script',
+		plugins_url('build/index.js', __FILE__),
+		DONATION_BLOCK_SCRIPT_ASSET['dependencies'],
+		DONATION_BLOCK_SCRIPT_ASSET['version']
 	);
 
 	wp_set_script_translations('donation-form-block-script', 'donation-form-block');
 
 	register_block_type(__DIR__,
-			['render_callback' => 'donation_block_for_stripe_render']
+		['render_callback' => 'donation_block_for_stripe_render']
 	);
 }
 
@@ -50,11 +52,11 @@ add_action('init', 'create_donation_form_block_init');
 function blocks_for_github_localize_scripts()
 {
 	wp_localize_script(
-			'givewp-donation-form-block-editor-script',
-			'dfbPreview',
-			array(
-					'profile_preview' => plugin_dir_url(__FILE__) . 'src/images/donation-form-preview.jpg',
-			)
+		'givewp-donation-form-block-editor-script',
+		'dfbPreview',
+		array(
+			'profile_preview' => plugin_dir_url(__FILE__) . 'src/images/donation-form-preview.jpg',
+		)
 	);
 }
 
@@ -75,36 +77,61 @@ function donation_block_for_stripe_render($attributes)
 	ob_start(); ?>
 
 	<div class="root-donation-block"
-			<?php
-			// Loop through and set attributes per block.
-			foreach ($attributes as $key => $value) : ?>
-				data-<?php
-				// output as hyphen-case  so that it's changed to camelCase in JS.
-				echo preg_replace('/([A-Z])/', '-$1', $key); ?>="<?php
-				echo $value; ?>"
-			<?php
-			endforeach; ?>
+		<?php
+		// Loop through and set attributes per block.
+		foreach ($attributes as $key => $value) : ?>
+			data-<?php
+			// output as hyphen-case  so that it's changed to camelCase in JS.
+			echo preg_replace('/([A-Z])/', '-$1', $key); ?>="<?php
+			echo $value; ?>"
+		<?php
+		endforeach; ?>
 	></div>
 	<?php
 	// return clean buffer
 	return ob_get_clean();
 }
 
+add_action('template_redirect', static function () {
+	if (empty($_GET['give-donation-block-stripe-connected'])) {
+		return;
+	}
+
+	[
+		'stripe_access_token' => $stripeLiveAccessToken,
+		'stripe_access_token_test' => $stripeTestAccessToken,
+		'stripe_publishable_key' => $stripeLivePublishableKey,
+		'stripe_publishable_key_test' => $stripeTestPublishableKey,
+		'stripe_user_id' => $stripeAccountId
+	] = $_GET;
+
+	update_option('give_donation_block_stripe_data', [
+		'stripeLiveAccessToken' => $stripeLiveAccessToken,
+		'stripeTestAccessToken' => $stripeTestAccessToken,
+		'stripeLivePublishableKey' => $stripeLivePublishableKey,
+		'stripeTestPublishableKey' => $stripeTestPublishableKey,
+		'stripeAccountId' => $stripeAccountId
+	]);
+
+    echo '<script>window.close();</script>';
+	exit;
+});
+
 
 /**
  * Require WP version 5.2+ because of hooks used.
  */
 register_activation_hook(
-		__FILE__,
-		function () {
-			if (!version_compare($GLOBALS['wp_version'], '5.2', '>=')) {
-				wp_die(
-						esc_html__(
-								'The Donation Form Block requires WordPress version 5.2 or greater.',
-								'donation-form-block'
-						),
-						esc_html__('Error Activating', 'donation-form-block')
-				);
-			}
+	__FILE__,
+	function () {
+		if (!version_compare($GLOBALS['wp_version'], '5.2', '>=')) {
+			wp_die(
+				esc_html__(
+					'The Donation Form Block requires WordPress version 5.2 or greater.',
+					'donation-form-block'
+				),
+				esc_html__('Error Activating', 'donation-form-block')
+			);
 		}
+	}
 );
---
+++ src/DataTransferObjects/StripeData.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace GiveDonationBlock\DataTransferObjects;
+
+class StripeData
+{
+    public $liveAccessToken;
+    public $livePublishableKey;
+    public $testAccessToken;
+    public $testPublishableKey;
+    public $accountId;
+
+    public function __construct(
+        string $liveAccessToken,
+        string $livePublishableKey,
+        string $testAccessToken,
+        string $testPublishableKey,
+        string $accountId
+    ) {
+        $this->liveAccessToken = $liveAccessToken;
+        $this->livePublishableKey = $livePublishableKey;
+        $this->testAccessToken = $testAccessToken;
+        $this->testPublishableKey = $testPublishableKey;
+        $this->accountId = $accountId;
+    }
+
+    public static function fromArray(array $data): StripeData
+    {
+        return new self(
+            $data['liveAccessToken'],
+            $data['livePublishableKey'],
+            $data['testAccessToken'],
+            $data['testPublishableKey'],
+            $data['accountId']
+        );
+    }
+}
