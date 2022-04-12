09edc471d7177172360926ffe7e870162a6b9ddc
devinwalker <sdtucson@gmail.com>
2022-04-11 19:36:07 -0700

    remove console log

--- src/helperFunctions/runLottieAnimation.js
+++ src/helperFunctions/runLottieAnimation.js
@@ -1,7 +1,6 @@
 import lottie from 'lottie-web';
 
 export default function runLottieAnimation(animationName, containerId) {
-    console.log(document.getElementById(containerId))
     import(`../lotties/${animationName}.json`).then((animationData) => {
         const animation = lottie.loadAnimation({
             container: document.getElementById(containerId),
